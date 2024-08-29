0e3561a7581fff544f979a76de5d4d6aa1cc6f10
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2024-08-28 21:01:12 -0300

    [M7.L1 · Actividad #3: "Bucles anidados" ]
    Implementamos bucles for anidados que dibujen nuestro mapa

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #pgzero
 
 """
-Version actual: [M7.L1 · Actividad #2: "Creando un mapa" ]
-Objetivo del ejercicio: comprender como modificar el tamaño del mapa y como vamos a cargar nuestros mapas (tablas)
+Version actual: [M7.L1 · Actividad #3: "Bucles anidados" ]
+Objetivo del ejercicio: Implementar bucles for anidados que dibujen nuestro mapa
+
+NOTA: Este ejercicio resuelve también la actividad #4, pero por conveniencia se explican ambos conceptos juntos
 
 Kodland: https://kenney.nl/assets/roguelike-caves-dungeons
 packs de assets: https://kenney.nl/assets/series:Tiny?sort=update
@@ -10,6 +12,12 @@ packs de assets: https://kenney.nl/assets/series:Tiny?sort=update
 
 # Ventana de juego hecha de celdas
 celda = Actor('border') # Celda que voy a utilizar como referencia para mi mapa
+
+# Paleta de terrenos:
+pared =  Actor("border") # 0: Pared de bloques
+piso =   Actor("floor")  # 1: Suelo liso (sin decoración)
+crack =  Actor("crack")  # 2: Suelo resquebrajado/quebradizo
+huesos = Actor("bones")  # 3: Suelo con una pilita de huesos
 
 cant_celdas_ancho = 7 # Ancho del mapa (en celdas)
 cant_celdas_alto = 7 # Altura del mapa (en celdas)
@@ -20,6 +28,8 @@ HEIGHT = celda.height * cant_celdas_alto  # Alto de la ventana (en píxeles)
 TITLE = "Rogue-like: Mazmorras" # Título de la ventana de juego
 FPS = 30 # Número de fotogramas por segundo
 
+################## MAPAS ##################
+
 mapa = [ [0, 0, 0, 0, 0, 0, 0],
          [0, 1, 2, 1, 3, 1, 0],
          [0, 1, 1, 2, 1, 1, 0],
@@ -27,3 +37,54 @@ mapa = [ [0, 0, 0, 0, 0, 0, 0],
          [0, 1, 1, 1, 3, 1, 0],
          [0, 1, 3, 1, 1, 2, 0],
          [0, 0, 0, 0, 0, 0, 0] ]
+
+mapa_2 = [[0, 0, 0, 0, 0, 0, 0],
+          [0, 1, 1, 3, 1, 1, 0],
+          [0, 1, 3, 1, 3, 1, 0],
+          [0, 3, 1, 1, 1, 3, 0],
+          [0, 3, 1, 1, 1, 3, 0],
+          [0, 1, 3, 3, 3, 1, 0],
+          [0, 0, 0, 0, 0, 0, 0]]
+
+###########################################
+
+mapa_actual = mapa # mapa a dibujar // cambiar valor si cambiamos de habitación
+
+def dibujar_mapa(mapa):
+
+  for fila in range(len(mapa)):
+    for columna in range(len(mapa[fila])):
+
+      """
+      Lista códigos terrenos
+      
+      0: pared
+      1: piso (sin nada)
+      2: piso (roto/resquebrajado)
+      3: piso (c/ huesitos)
+      """
+
+      if (mapa[fila][columna] == 0): # pared
+        pared.left = pared.width * columna
+        pared.top = pared.height * fila
+        pared.draw()
+
+      elif (mapa[fila][columna] == 1): # piso (sin nada)
+        piso.left = piso.width * columna
+        piso.top = piso.height * fila
+        piso.draw()
+
+      elif (mapa[fila][columna] == 2): # piso (roto/resquebrajado)
+        crack.left = crack.width * columna
+        crack.top = crack.height * fila
+        crack.draw()
+
+      elif (mapa[fila][columna] == 3): # piso (c/ huesitos)
+        huesos.left = huesos.width * columna
+        huesos.top = huesos.height * fila
+        huesos.draw()
+
+""" >>> Probamos dibujar mapa <<< """
+
+def update(dt):
+    dibujar_mapa(mapa_actual)
