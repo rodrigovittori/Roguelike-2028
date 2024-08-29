a3b38c5eb7fd6765968f643cc6272cb77a55bb60
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2024-08-28 21:33:29 -0300

    [M7.L1 · Actividades Extra]
    Ampliamos tamaño mapa, modificamos restricciones de movimiento y reubicamos texto de UI

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 #pgzero
 
 """
-Version actual: [M7.L1 · Actividad #6: "Desplazamiento a través de las celdas" ]
-Objetivo del ejercicio: Implementaremos nuestro sistema de movimiento (por casillas/por turnos)
+Version actual: [M7.L1 · Actividades Extra]
+Objetivo del ejercicio: Ampliar mapa, agregar fila extra y reubicar datos en pantalla
+
+Nota: Nuestro código ya cumple la primer actividad extra
 
 Pasos:
-#1: Agregamos nuestra función on_key_down(key)
+#1: Modificamos nuestro mapa para que sea de 9x9 casillas, más una fila extra para mostrar los atributos del jugador
+#2: Modificamos nuestros mapas
+#3: Agregamos el color de relleno de la pantalla en nuestro draw()
+#4: Modificamos los screen.draw.text()
+#5: Modificamos los límites de restricción de movimiento
 
 Kodland: https://kenney.nl/assets/roguelike-caves-dungeons
 packs de assets: https://kenney.nl/assets/series:Tiny?sort=update
@@ -20,8 +26,8 @@ piso =   Actor("floor")  # 1: Suelo liso (sin decoración)
 crack =  Actor("crack")  # 2: Suelo resquebrajado/quebradizo
 huesos = Actor("bones")  # 3: Suelo con una pilita de huesos
 
-cant_celdas_ancho = 7 # Ancho del mapa (en celdas)
-cant_celdas_alto = 7 # Altura del mapa (en celdas)
+cant_celdas_ancho = 9 # Ancho del mapa (en celdas)
+cant_celdas_alto = 10 # Altura del mapa (en celdas)
 
 WIDTH =  celda.width  * cant_celdas_ancho # Ancho de la ventana (en píxeles)
 HEIGHT = celda.height * cant_celdas_alto  # Alto de la ventana (en píxeles)
@@ -40,21 +46,28 @@ personaje.ataque = 5
 
 ################## MAPAS ##################
 
-mapa = [ [0, 0, 0, 0, 0, 0, 0],
-         [0, 1, 2, 1, 3, 1, 0],
-         [0, 1, 1, 2, 1, 1, 0],
-         [0, 3, 2, 1, 1, 3, 0],
-         [0, 1, 1, 1, 3, 1, 0],
-         [0, 1, 3, 1, 1, 2, 0],
-         [0, 0, 0, 0, 0, 0, 0] ]
+mapa =   [[0, 0, 0, 0, 0, 0, 0, 0, 0], 
+          [0, 1, 1, 1, 1, 1, 1, 1, 0], 
+          [0, 1, 1, 2, 1, 3, 1, 1, 0], 
+          [0, 1, 1, 1, 2, 1, 1, 1, 0], 
+          [0, 1, 3, 2, 1, 1, 3, 1, 0], 
+          [0, 1, 1, 1, 1, 3, 1, 1, 0], 
+          [0, 1, 1, 3, 1, 1, 2, 1, 0], 
+          [0, 1, 1, 1, 1, 1, 1, 1, 0], 
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [-1, -1, -1, -1, -1, -1, -1, -1, -1] ] # Fila extra para mostrar el texto
 
-mapa_2 = [[0, 0, 0, 0, 0, 0, 0],
-          [0, 1, 1, 3, 1, 1, 0],
-          [0, 1, 3, 1, 3, 1, 0],
-          [0, 3, 1, 1, 1, 3, 0],
-          [0, 3, 1, 1, 1, 3, 0],
-          [0, 1, 3, 3, 3, 1, 0],
-          [0, 0, 0, 0, 0, 0, 0]]
+mapa_2 = [ [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+           [0, 1, 1, 1, 1, 1, 1, 1, 0], 
+           [0, 1, 1, 3, 1, 3, 1, 1, 0], 
+           [0, 1, 1, 3, 1, 3, 1, 1, 0], 
+           [0, 1, 1, 1, 1, 1, 1, 1, 0], 
+           [0, 3, 1, 1, 1, 1, 1, 3, 0], 
+           [0, 1, 3, 1, 1, 1, 3, 1, 0], 
+           [0, 1, 1, 3, 3, 3, 1, 1, 0], 
+           [0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [-1, -1, -1, -1, -1, -1, -1, -1, -1] ] # Fila extra para mostrar el texto
+
 
 ###########################################
 
@@ -99,11 +112,12 @@ def dibujar_mapa(mapa):
   #####################  """
 
 def draw():
+  screen.fill("#2f3542") # rgb = (47, 53, 66)
   dibujar_mapa(mapa_actual)
   personaje.draw()
     
-  screen.draw.text(("PS: " + str(personaje.salud)), midright=((WIDTH - 15), 14), color = 'white', fontsize = 16)
-  screen.draw.text(("ATK: " + str(personaje.ataque)), midright=((WIDTH - 15), 36), color = 'white', fontsize = 16)
+  screen.draw.text(("Salud: " + str(personaje.salud)), midleft=(30, (HEIGHT - int(celda.height/2))), color = 'white', fontsize = 24)
+  screen.draw.text(("Ataque: " + str(personaje.ataque)), midright=((WIDTH - 30), (HEIGHT - int(celda.height/2))), color = 'white', fontsize = 24)
 
 def on_key_down(key):
   
@@ -116,8 +130,8 @@ def on_key_down(key):
     personaje.x -= celda.width
     personaje.image = "left" # xq mira a la izq
         
-  elif ((keyboard.down or keyboard.s) and (personaje.y < HEIGHT - celda.height * 2)):
-    # ¿Xq 2?: Una (a la que me voy a desplazar) y otra (por la pared, que NO puedo atravesar)
+  elif ((keyboard.down or keyboard.s) and (personaje.y < HEIGHT - celda.height * 3)):
+    # ¿Xq 3?: Una (a la que me voy a desplazar), otra (por la pared, que NO puedo atravesar) Y UNA TERCERA (para mostrar el texto)
     personaje.y += celda.height
     
   elif ((keyboard.up or keyboard.w) and (personaje.y > (celda.height * 2))):
